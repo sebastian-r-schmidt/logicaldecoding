ba69e79114177ca87033298fcfb3b0bbf2446b36
Sebastian Schmidt <schmidt.sebastian2@swm.de>
2015-11-05 13:01:12 +0100

    initiale Projektgenerierung (aus start.spring.io)

---
+++ src/main/java/de/swm/nis/RefreshCacheApplication.java
@@ -0,0 +1,12 @@
+package de.swm.nis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefreshCacheApplication.class, args);
+    }
+}
