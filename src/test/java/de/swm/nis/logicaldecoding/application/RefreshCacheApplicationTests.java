467183f35ebfb9dfa1b1574233235e2b3f3cc09c
Sebastian Schmidt <Schmidt.Sebastian2@swm.de>
2016-08-18 09:55:36 +0200

    Migrate to Spring Boot 1.4.0

--- src/test/java/de/swm/nis/logicaldecoding/application/RefreshCacheApplicationTests.java
+++ src/test/java/de/swm/nis/logicaldecoding/application/RefreshCacheApplicationTests.java
@@ -23,13 +23,13 @@ package de.swm.nis.logicaldecoding.application;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import de.swm.nis.logicaldecoding.RefreshCacheApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = RefreshCacheApplication.class)
+@SpringBootTest(classes = RefreshCacheApplication.class)
 public class RefreshCacheApplicationTests {
 
 	@Test
